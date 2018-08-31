7b0d64ea8fe685223b439319acb6dd4f588c3acf
Abhivarsh Peddireddy <pabhivarshnv@gmail.com>
2018-08-31 11:38:03 -0400

    initial commit

---
+++ homework_01/app/src/main/java/com/example/pabhivarshnv/baccalculator/MainActivity.java
@@ -0,0 +1,194 @@
+package com.example.pabhivarshnv.baccalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText weight;
+    Switch gender;
+    RadioGroup drinksRgp;
+    RadioButton drinkSizeRb;
+    SeekBar alcoholPercentage;
+    TextView bacValue;
+    TextView status;
+    TextView alcoholIndicator;
+    ProgressBar progressBar;
+    CharSequence genderValue = "F";
+    Double weightValue;
+    int drinkSize=1;
+    int alcoholPercentageIndicator=5;
+    double calBacValue;
+    double val=0.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.glass_icon);
+        actionBar.setTitle("  BAC Calculator");
+
+        weight = findViewById(R.id.input_weight);
+        gender = (Switch) findViewById(R.id.gender);
+        drinksRgp = (RadioGroup) findViewById(R.id.alcohol_size);
+        drinkSizeRb = (RadioButton) findViewById(drinksRgp.getCheckedRadioButtonId());
+        alcoholPercentage = (SeekBar) findViewById(R.id.seekBar_alcohol);
+        bacValue = findViewById(R.id.bac_value);
+        status = findViewById(R.id.status_value);
+        alcoholIndicator = findViewById(R.id.alcohol_indicator);
+        progressBar = findViewById(R.id.progressBar_bac);
+        alcoholIndicator.setText("5%");
+        bacValue.setText("0.00");
+        status.setText("You're safe");
+
+        gender.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    genderValue = gender.getTextOn();
+                }
+                else{
+                    genderValue = gender.getTextOff();
+                }
+            }
+        });
+
+        drinksRgp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                drinkSizeRb = findViewById(checkedId);
+                drinkSize = Integer.parseInt(drinkSizeRb.getText().toString().split(" ")[0]);
+            }
+        });
+
+        findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(weight.getText().toString().equalsIgnoreCase("")
+                        ||Double.parseDouble(weight.getText().toString()) <= 0.0){
+                    Toast.makeText(MainActivity.this, "Please enter weight", Toast.LENGTH_SHORT).show();
+                    weight.setText("");
+                }else {
+                    weightValue = Double.parseDouble(weight.getText().toString());
+                    Log.d("demo", "weight : "+weightValue+",gender : "+genderValue);
+                    Toast.makeText(MainActivity.this, "weight and gender have been saved!!", Toast.LENGTH_SHORT).show();
+                    calculateBac();
+
+                }
+            }
+        });
+
+        alcoholPercentage.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("demo", "onProgressChanged: "+progress);
+                alcoholPercentageIndicator=progress*5;
+                alcoholIndicator.setText(progress*5+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        findViewById(R.id.add_drink).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (setError(this)){
+                    calculateBac();
+                }
+
+            }
+        });
+
+        findViewById(R.id.reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weight.setFocusable(true);
+                gender.setClickable(true);
+                findViewById(R.id.shot).setClickable(true);
+                findViewById(R.id.glass).setClickable(true);
+                findViewById(R.id.can).setClickable(true);
+                findViewById(R.id.add_drink).setEnabled(true);
+                findViewById(R.id.save_button).setEnabled(true);
+                findViewById(R.id.seekBar_alcohol).setEnabled(true);
+
+                weight.setText("");
+                gender.setChecked(false);
+                drinksRgp.check(R.id.shot);
+                alcoholPercentage.setProgress(1);
+
+            }
+        });
+
+    }
+
+    private void calculateBac() {
+        double g = genderValue.equals("F")?0.55:0.68;
+        calBacValue = (drinkSize*(alcoholPercentageIndicator)*6.24/(weightValue*g))/100;
+        val += (double) Math.round(calBacValue*1000);
+        bacValue.setText(Double.toString(val/1000));
+        Log.d("demo", "onClick: "+drinkSize+" : "+alcoholPercentageIndicator+" : "+
+                weightValue+" : "+g+" : "+val+" : "+calBacValue);
+        progressBar.setProgress((int) Math.round(val/10));
+
+        if(val/1000>0.08 && val/1000<0.20){
+            status.setText("Be careful...");
+            status.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+        }
+        else if(val/1000>=0.20){
+            status.setText("Over the limit!");
+            status.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
+        }
+
+        if((val/10)>=25.0){
+            weight.setFocusable(false);
+            gender.setClickable(false);
+            findViewById(R.id.shot).setClickable(false);
+            findViewById(R.id.glass).setClickable(false);
+            findViewById(R.id.can).setClickable(false);
+            findViewById(R.id.seekBar_alcohol).setEnabled(false);
+            findViewById(R.id.add_drink).setEnabled(false);
+            findViewById(R.id.save_button).setEnabled(false);
+
+        }
+    }
+
+    public boolean setError(View.OnClickListener view) {
+
+        try{
+            if (weightValue.equals("") || weightValue.equals(" ") || weightValue<=0.0){
+                Toast.makeText(this, "Enter the Weight in lb", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        catch (Exception e){
+            Toast.makeText(this, "Enter the Weight in lb", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+}
